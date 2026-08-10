670b3ef1e0838cd6d105e774ad6f3db2e13228c8
lazzari <enrico.lazzari@sap.com>
2026-08-10 18:30:54 -0300

    feat: added java version println

--- arquivoJava.java
+++ arquivoJava.java
@@ -3,6 +3,8 @@ public class arquivoJava {
     public static void main(String[] args) throws Exception {
         System.out.println("\nHi!");
         String version = System.getProperty("java.version");
+        String osVersion = System.getProperty("os.version");
         System.out.println("Running Java Version "+version+"\n");
+        System.out.println("Operational System in use: " + osVersion + "" );
     }
 }
