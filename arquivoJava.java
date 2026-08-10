b429ba7eb4c8c67e0e943d1087d5a0c8c6ade6a0
lazzari <enrico.lazzari@sap.com>
2026-08-10 18:27:18 -0300

    First Commit YEAH

---
+++ arquivoJava.java
@@ -0,0 +1,8 @@
+
+public class arquivoJava {
+    public static void main(String[] args) throws Exception {
+        System.out.println("\nHi!");
+        String version = System.getProperty("java.version");
+        System.out.println("Running Java Version "+version+"\n");
+    }
+}
