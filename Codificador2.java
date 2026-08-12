34ceeecc8e00a98272c673ee6dfce34d20bd55a4
Cristiammassens <cristiammassens13@gmail.com>
2026-08-12 21:45:14 +0000

    alteração no codificador2

--- Codificador2.java
+++ Codificador2.java
@@ -1,6 +1,7 @@
 import java.time.LocalDate;
 
 public class Codificador2 implements Codificador {
+    private static final int DESLOCAMENTO = 3; 
     public String getNome() {
         return "Codificador2";
     }
@@ -10,13 +11,13 @@ public class Codificador2 implements Codificador {
     }
 
     public int getNivelSeguranca(){
-        return 15;
+        return 10;
     }
 
     public String codifica2(String str) {
         String invertida = new StringBuilder(str).reverse().toString();
  
-        // 2) desloca cada caractere
+    
         StringBuilder codificado = new StringBuilder();
         for (char c : invertida.toCharArray()) {
             codificado.append((char) (c + DESLOCAMENTO));
@@ -26,13 +27,11 @@ public class Codificador2 implements Codificador {
     }
 
     public String decodifica2(String str) {
-        // desfaz o deslocamento primeiro
         StringBuilder semDeslocamento = new StringBuilder();
         for (char c : str.toCharArray()) {
             semDeslocamento.append((char) (c - DESLOCAMENTO));
         }
  
-        // depois desfaz a inversão
         return semDeslocamento.reverse().toString();
     }
     
