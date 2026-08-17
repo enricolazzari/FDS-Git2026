3cdfd34624ca8e4b61d994de7f372fcfbd7ecae8
lazzari <enrico.lazzari@sap.com>
2026-08-17 18:09:22 -0300

    feat/ criar um exemplo de uso do “Factory” que demonstre as funcionalidades de todas as implementações da interface “Codificador”

--- Codificador2.java
+++ Codificador2.java
@@ -26,13 +26,29 @@ public class Codificador2 implements Codificador {
         return codificado.toString();
     }
 
-    public String decodifica2(String str) {
+    public String descodifica2(String str) {
         StringBuilder semDeslocamento = new StringBuilder();
         for (char c : str.toCharArray()) {
             semDeslocamento.append((char) (c - DESLOCAMENTO));
         }
- 
+
         return semDeslocamento.reverse().toString();
     }
-    
+
+    public String codifica(String str) {
+        throw new UnsupportedOperationException("codifica nao suportado por Codificador2");
+    }
+
+    public String decodifica(String str) {
+        throw new UnsupportedOperationException("decodifica nao suportado por Codificador2");
+    }
+
+    public String codifica1(String str) {
+        throw new UnsupportedOperationException("codifica1 nao suportado por Codificador2");
+    }
+
+    public String descodifica1(String str) {
+        throw new UnsupportedOperationException("descodifica1 nao suportado por Codificador2");
+    }
+
 }
